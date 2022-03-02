9a6d6b865258dff48414d74e46025d5b32977c5d
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-03-03 02:54:24 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     publicRuntimeConfig: {
         APP_NAME: 'Top Dish',
         API: 'http://localhost:8000/api',
-        PRODUCTION: false,
+        PRODUCTION: true,
         DOMAIN: 'http://localhost:3000',
     }
 };
