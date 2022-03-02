e946d02d8e1ac7f0875cf47bc8ec05c062e10f93
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-03-03 02:35:25 +0500

    f

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    publicRuntimeConfig: {
+        APP_NAME: 'Top Dish',
+        API: 'http://localhost:8000/api',
+        PRODUCTION: false,
+        DOMAIN: 'http://localhost:3000',
+    }
+};
