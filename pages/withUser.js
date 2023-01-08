93d2af7c14f42b2725b26bd8708522d502ccbd7b
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2023-01-09 00:16:13 +0500

    Deployment fix

--- pages/withUser.js
+++ pages/withUser.js
@@ -1,61 +1,62 @@
-import axios from 'axios';
-import  Router from 'next/router';
+import axios from "axios";
+import Router from "next/router";
 // import { API } from '../config';
-import { getCookie } from '../helpers/auth';
+import { getCookie } from "../helpers/auth";
 
+const withUser = (Page) => {
+  const API = "https://puzzled-gabardine-clam.cyclic.app/";
+  //   const API = "http://localhost:8000/api";
 
-const withUser = Page => {
-     const API = "https://top-dish.herokuapp.com/api"
-    const APP_NAME = "Top Dish"
-    const WithAuthUser = props => <Page {...props} />;
-    WithAuthUser.getInitialProps = async context => {
-        const token = getCookie('token', context.req);
-        let user = null;
-        let userLinks = [];
+  const APP_NAME = "Top Dish";
+  const WithAuthUser = (props) => <Page {...props} />;
+  WithAuthUser.getInitialProps = async (context) => {
+    const token = getCookie("token", context.req);
+    let user = null;
+    let userLinks = [];
 
-        if (token) {
-            try {
-                const response = await axios.get(`${API}/user`, {
-                    headers: {
-                        authorization: `Bearer ${token}`,
-                        contentType: 'application/json'
-                    }
-                });
-                console.log('response in withUser', response);
-                user = response.data.user;
-                userLinks = response.data.links;
-            } catch (error) {
-                if (error.response.status === 401) {
-                    user = null;
-                }
-            }
+    if (token) {
+      try {
+        const response = await axios.get(`${API}/user`, {
+          headers: {
+            authorization: `Bearer ${token}`,
+            contentType: "application/json",
+          },
+        });
+        console.log("response in withUser", response);
+        user = response.data.user;
+        userLinks = response.data.links;
+      } catch (error) {
+        if (error.response.status === 401) {
+          user = null;
         }
+      }
+    }
 
-        if (user === null) {
-        //   if (context.res) { // server
-        //  return  context.res.writeHead(302, {
-        //         Location: '/'
-        //     });
-        
-        //     } else { // client
-        //    return Router.push('/');
-        //     }
-           // redirect
-            context.res.writeHead(302, {
-                Location: '/'
-            });
-            context.res.end();
-        } else {
-            return {
-                ...(Page.getInitialProps ? await Page.getInitialProps(context) : {}),
-                user,
-                token,
-                userLinks
-            };
-        }
-    };
+    if (user === null) {
+      //   if (context.res) { // server
+      //  return  context.res.writeHead(302, {
+      //         Location: '/'
+      //     });
 
-    return WithAuthUser;
+      //     } else { // client
+      //    return Router.push('/');
+      //     }
+      // redirect
+      context.res.writeHead(302, {
+        Location: "/",
+      });
+      context.res.end();
+    } else {
+      return {
+        ...(Page.getInitialProps ? await Page.getInitialProps(context) : {}),
+        user,
+        token,
+        userLinks,
+      };
+    }
+  };
+
+  return WithAuthUser;
 };
 
 export default withUser;
